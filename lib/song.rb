e686b0581bfc59647cb4645229e474575012e5c4
Marcus Lau <lau.marcus76@gmail.com>
2020-04-29 20:05:38 +0000

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,60 @@
+class Song
+  
+  @@count=0
+  @@genres = [] # this contains duplicates of song genres
+  @@artists = [] # contains dups of artists 
+  
+  attr_accessor :name, :artist, :genre
+  
+  def initialize(name, artist, genre)
+    @@count+=1
+    @name = name
+    @artist = artist
+    @genre = genre
+    @@artists << artist
+    @@genres << genre
+  end
+  
+  def self.count 
+    @@count
+  end
+  
+  def self.genres
+    @@genres.uniq 
+  end
+  
+  def self.artists
+    @@artists.uniq
+  end
+  
+  def self.genre_count
+    genre_count = Hash.new
+    @@genres.each do |genre|
+      if genre_count[genre]
+        genre_count[genre]+=1
+      else
+        genre_count[genre] = 1
+      end
+    end
+    genre_count
+  end
+  
+  def self.artist_count
+    artist_count = Hash.new
+    @@artists.each do |artist|
+      if artist_count[artist]
+        artist_count[artist]+=1
+      else
+        artist_count[artist] = 1
+      end
+    end
+    artist_count
+  end
+  
+  
+end
+
+
+
+
+
